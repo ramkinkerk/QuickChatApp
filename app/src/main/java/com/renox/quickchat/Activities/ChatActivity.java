7e1d80dfb62f902d3daadf90ffaff28815cfe277
Ramkinker Kumar <roxtarrk1997@gmail.com>
2021-09-06 03:11:19 +0530

    first commit

---
+++ app/src/main/java/com/renox/quickchat/Activities/ChatActivity.java
@@ -0,0 +1,116 @@
+package com.renox.quickchat.Activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.renox.quickchat.Adapter.MessagesAdapter;
+import com.renox.quickchat.Model.Message;
+import com.renox.quickchat.databinding.ActivityChatBinding;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class ChatActivity extends AppCompatActivity {
+
+    ActivityChatBinding binding;
+    MessagesAdapter adapter;
+    ArrayList<Message> messages;
+    String senderRoom, receiverRoom;
+    FirebaseDatabase database;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityChatBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        messages = new ArrayList<>();
+        adapter = new MessagesAdapter(this,messages, senderRoom, receiverRoom);
+
+        binding.RecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        binding.RecyclerView.setAdapter(adapter);
+
+        String name = getIntent().getStringExtra("name");
+        String ReceiverUid = getIntent().getStringExtra("uid");
+        String SenderUid = FirebaseAuth.getInstance().getUid();
+
+        senderRoom = SenderUid + ReceiverUid;
+        receiverRoom = ReceiverUid + SenderUid;
+
+        database = FirebaseDatabase.getInstance();
+
+        database.getReference().child("chats")
+                .child(senderRoom)
+                .child("messages")
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        messages.clear();
+                        for (DataSnapshot snapshot1 : snapshot.getChildren()){
+                            Message message = snapshot1.getValue(Message.class);
+                            message.setMessageId(snapshot1.getKey());
+                            messages.add(message);
+                        }
+                        adapter.notifyDataSetChanged();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
+        binding.sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String messageTxt = binding.messageBox.getText().toString();
+
+                Date date = new Date();
+                Message message = new Message(messageTxt, SenderUid, date.getTime());
+                binding.messageBox.setText("");
+
+                String randomKey = database.getReference().push().getKey();
+
+                database.getReference().child("chats")
+                        .child(senderRoom)
+                        .child("messages")
+                        .child(randomKey)
+                        .setValue(message).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        database.getReference().child("chats")
+                                .child(receiverRoom)
+                                .child("messages")
+                                .child(randomKey)
+                                .setValue(message).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+
+                            }
+                        });
+
+                    }
+                });
+            }
+        });
+
+        getSupportActionBar().setTitle(name);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return super.onSupportNavigateUp();
+    }
+}
---
+++ app/src/main/java/com/renox/quickchat/Activities/MainActivity.java
@@ -0,0 +1,79 @@
+package com.renox.quickchat.Activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Switch;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.renox.quickchat.Adapter.UsersAdapter;
+import com.renox.quickchat.Model.User;
+import com.renox.quickchat.R;
+import com.renox.quickchat.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    FirebaseDatabase database;
+    ArrayList<User> users;
+    UsersAdapter usersAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        database = FirebaseDatabase.getInstance();
+        users = new ArrayList<>();
+        usersAdapter = new UsersAdapter(this,users);
+        binding.recyclerView.setAdapter(usersAdapter);
+
+        database.getReference().child("users").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                users.clear();
+                for (DataSnapshot snapshot1 : snapshot.getChildren()){
+                    User user = snapshot1.getValue(User.class);
+                    users.add(user);
+                }
+                usersAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.search:
+                break;
+            case R.id.settings:
+
+                break;
+            case R.id.groups:
+
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.top_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
